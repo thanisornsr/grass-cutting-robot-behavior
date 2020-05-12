2a8a4c8dfb8596cccaed80ddefc34a87c7485548
ThanisornSr <thanisornsr@gmail.com>
2020-05-12 23:13:34 +0700

    Parameter changed

--- tme290-lawnmower-master/src/tme290-lawnmower.cpp
+++ tme290-lawnmower-master/src/tme290-lawnmower.cpp
@@ -91,7 +91,7 @@ void foo(){
   // Maximum charge
   myMaximumCharge = 0.98f;
   // Battery drain per step
-  myBatteryDrainRate = 0.011f;
+  myBatteryDrainRate = 0.009f;
   // Cutting Target
   myTargetCut = 0.3f;
   // Max step of rainning before go home
